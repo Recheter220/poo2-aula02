43db2033f315e0e3d101163f23e2f2b1ec8cff7b
Recheter220 <recheter220@gmail.com>
2018-03-06 18:13:28 -0300

    Exercício E finalizado e comentado.

--- Exercícios/ExerciciosAula02/ExerciciosAula02/MenuPrincipal.cs
+++ Exercícios/ExerciciosAula02/ExerciciosAula02/MenuPrincipal.cs
@@ -16,5 +16,10 @@ namespace ExerciciosAula02
         {
             InitializeComponent();
         }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            (new exerc_e()).ShowDialog();
+        }
     }
 }
