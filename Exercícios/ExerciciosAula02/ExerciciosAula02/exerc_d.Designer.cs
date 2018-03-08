b5d294987318243598c030226587c97890779f20
Samira <33137790+samiraald@users.noreply.github.com>
2018-03-08 11:04:56 -0300

    Add files via upload

--- Exercícios/ExerciciosAula02/ExerciciosAula02/exerc_d.Designer.cs
+++ Exercícios/ExerciciosAula02/ExerciciosAula02/exerc_d.Designer.cs
@@ -36,6 +36,8 @@
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
             this.textBox2 = new System.Windows.Forms.TextBox();
+            this.dgvaluno = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dgvnota = new System.Windows.Forms.DataGridViewTextBoxColumn();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
             this.SuspendLayout();
@@ -43,74 +45,98 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(24, 41);
+            this.label1.Location = new System.Drawing.Point(36, 63);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(82, 13);
+            this.label1.Size = new System.Drawing.Size(120, 20);
             this.label1.TabIndex = 0;
             this.label1.Text = "Nome do aluno:";
             // 
             // dataGridView1
             // 
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(27, 143);
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.dgvaluno,
+            this.dgvnota});
+            this.dataGridView1.Location = new System.Drawing.Point(40, 220);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(240, 150);
+            this.dataGridView1.Size = new System.Drawing.Size(360, 231);
             this.dataGridView1.TabIndex = 2;
+           // this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
             // 
             // dataGridView2
             // 
             this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView2.Location = new System.Drawing.Point(399, 143);
+            this.dataGridView2.Location = new System.Drawing.Point(598, 220);
+            this.dataGridView2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.dataGridView2.Name = "dataGridView2";
-            this.dataGridView2.Size = new System.Drawing.Size(240, 150);
+            this.dataGridView2.Size = new System.Drawing.Size(360, 231);
             this.dataGridView2.TabIndex = 3;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(67, 92);
+            this.button1.Location = new System.Drawing.Point(100, 142);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(152, 23);
+            this.button1.Size = new System.Drawing.Size(228, 35);
             this.button1.TabIndex = 4;
             this.button1.Text = "Armazenar Informação";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(439, 92);
+            this.button2.Location = new System.Drawing.Point(658, 142);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(142, 23);
+            this.button2.Size = new System.Drawing.Size(213, 35);
             this.button2.TabIndex = 5;
             this.button2.Text = "Filtrar notas abaixo de 6,0";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(112, 38);
+            this.textBox1.Location = new System.Drawing.Point(168, 58);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(353, 20);
+            this.textBox1.Size = new System.Drawing.Size(528, 26);
             this.textBox1.TabIndex = 6;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(488, 41);
+            this.label2.Location = new System.Drawing.Point(732, 63);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(33, 13);
+            this.label2.Size = new System.Drawing.Size(47, 20);
             this.label2.TabIndex = 7;
             this.label2.Text = "Nota:";
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(527, 38);
+            this.textBox2.Location = new System.Drawing.Point(790, 58);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(72, 20);
+            this.textBox2.Size = new System.Drawing.Size(106, 26);
             this.textBox2.TabIndex = 8;
+            // 
+            // dgvaluno
+            // 
+            this.dgvaluno.HeaderText = "Aluno";
+            this.dgvaluno.Name = "dgvaluno";
+            // 
+            // dgvnota
+            // 
+            this.dgvnota.HeaderText = "Nota";
+            this.dgvnota.Name = "dgvnota";
             // 
             // exerc_d
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(722, 352);
+            this.ClientSize = new System.Drawing.Size(1083, 542);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.textBox1);
@@ -119,8 +145,10 @@
             this.Controls.Add(this.dataGridView2);
             this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "exerc_d";
             this.Text = "exerc_d";
+            this.Load += new System.EventHandler(this.exerc_d_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
             this.ResumeLayout(false);
@@ -138,6 +166,8 @@
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dgvaluno;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dgvnota;
     }
 }
 
--- Exercícios/ExerciciosAula02/ExerciciosAula02/exerc_d.cs
+++ Exercícios/ExerciciosAula02/ExerciciosAula02/exerc_d.cs
@@ -12,9 +12,77 @@ namespace ExerciciosAula02
 {
     public partial class exerc_d : Form
     {
+        List<Notas> listaNota;
+
         public exerc_d()
         {
             InitializeComponent();
         }
+        private void exerc_e_Load(object sender, EventArgs e)
+        {
+            listaNota = new List<Notas>();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+                /* Atribuições de variáveis */
+                string aluno = textBox1.Text;
+                double nota = 0;
+                double.TryParse(textBox2.Text, System.Globalization.NumberStyles.Currency, System.Globalization.CultureInfo.CurrentUICulture, out nota);
+            
+            /* Procura no DGV se já existe um cliente com o ID que está tentando ser inserido */
+                foreach (DataGridViewRow rw in dataGridView1.Rows)
+            {
+                if ( (rw.Cells[dgvaluno.Index].Value.ToString()) == aluno)
+                {
+                    /* Caso exista, exibe uma mensagem de erro */
+                    MessageBox.Show("Já existe um aluno com esse nome cadastrado, tente novamente.", "Duplicidade", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    textBox1.Focus();
+                    return;
+                }
+            }
+            /* Caso contrário, adiciona a linha ao DGV */
+            dataGridView1.Rows.Add(aluno, nota, "Excluir");
+        }
+            catch (Exception trambolho)
+            {
+                MessageBox.Show("Erro de caracter "+trambolho.Message);
+            }
+        }
+        private void exerc_d_Load(object sender, EventArgs e)
+        {
+
+         
+
+            
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            /* Recupera os dados do produto selecionado */
+            int linha = dataGridView1.SelectedCells[0].RowIndex;
+            string nomealuno = dataGridView1[dgvaluno.Index, linha].Value.ToString();
+            double notaanterior = Convert.ToDouble(dataGridView1[dgvnota.Index, linha].Value.ToString());
+
+            /* Em caso da quantidade a ser comprada ser maior que o estoque, confirmar com o usuário */
+            if (notaanterior >6)
+            {
+                   return;
+                }
+            }
+
+        
     }
-}
+    }
+
+        
+    
+
+    internal class Notas
+    {
+    }
+
